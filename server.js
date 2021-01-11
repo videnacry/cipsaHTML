e30b57bddbb6f0ca28ba93b940963f006a9adabc
beron <bleink@outlook.es>
2021-01-11 23:24:36 +0100

    server.js - add route for exercise 6 and static folder [porfolio, images]

--- server.js
+++ server.js
@@ -5,8 +5,13 @@ import express from 'express'
 const app = express()
 
 app.use(express.static('style'))
+app.use('/portfolio', express.static('portfolio'))
+app.use('/images', express.static('imagenes'))
 app.set('view engine', 'ejs')
 
+app.get('/exercise/6', (req, res) => {
+    res.sendFile(join(process.cwd(), 'index.html'))
+})
 app.get('/exercise/:num', (req, res) => {
     res.render('index', {exercisePath: './exercise' + req.params.num})
 })
