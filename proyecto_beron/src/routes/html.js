cfdb9727a079bd8061ee11a994ea886df4f01bb8
beron <bleink@outlook.es>
2021-01-29 00:14:59 +0100

    replace header route per nav

--- proyecto_beron/src/routes/html.js
+++ proyecto_beron/src/routes/html.js
@@ -3,15 +3,15 @@ import express from 'express'
 
 const route = express.Router()
 
-route.get('/header', (req, res) => {
-    if (existsSync('./proyecto_beron/src/assets/header.html')) {
-        const html = readFileSync('./proyecto_beron/src/assets/header.html', {encoding: 'utf-8'})
+route.get('/nav', (req, res) => {
+    if (existsSync('./proyecto_beron/src/assets/nav.html')) {
+        const html = readFileSync('./proyecto_beron/src/assets/nav.html', {encoding: 'utf-8'})
         res.status(200).json({
             html
         })
     } else {
         res.status(500).json({
-            err: 'header.html not found in proyecto_beron'
+            err: 'nav.html not found in proyecto_beron'
         })
     }
 })
