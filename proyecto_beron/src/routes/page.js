d42d3be71d51abfb7932c236d34e0a3c5ec96399
beron <bleink@outlook.es>
2021-01-29 01:15:10 +0100

    page.js - create routes per section for project_beron

---
+++ proyecto_beron/src/routes/page.js
@@ -0,0 +1,19 @@
+import express from 'express'
+import { join } from 'path'
+
+const route = express.Router()
+
+route.get('/coexistence', (req, res) => {
+    res.sendFile(join(process.cwd(), 'proyecto_beron', 'coexistence.html'))
+})
+route.get('/contacto', (req, res) => {
+    res.sendFile(join(process.cwd(), 'proyecto_beron', 'contacto.html'))
+})
+route.get('/rest', (req, res) => {
+    res.sendFile(join(process.cwd(), 'proyecto_beron', 'rest.html'))
+})
+route.get('/initiative', (req, res) => {
+    res.sendFile(join(process.cwd(), 'proyecto_beron', 'initiative.html'))
+})
+
+export const pageRoute = route
